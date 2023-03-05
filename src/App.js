08e5a6a54cc96f14efaebabba463384525a8f30e
Arturo Serrato <arturo.serrato@yahoo.com>
2023-03-05 15:05:48 -0800

    updated photo's

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import About from './components/About';
-import Portfolio from './components/Portfolio';
-import Resume from './components/Resume';
+//import Portfolio from './components/Portfolio';
+//import Resume from './components/Resume';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 import './index.css';
@@ -16,8 +16,6 @@ function App() {
                 <Routes>
                     <Route path='/' element={<About/>} />
                     <Route path='/aboutme' element={<About />}/>
-                    <Route path='/portfolio' element={<Portfolio />}/>
-                    <Route path='/resume' element={<Resume />}/>
                     <Route path='/contact' element={<Contact />}/>
                     <Route path='*' element={<About />}/>
                     </Routes>
